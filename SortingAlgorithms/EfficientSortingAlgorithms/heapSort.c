9f5b81d4a6deaaaf8910cd9500df63a032b7b503
Pedr0 <64512195+d1zm4as@users.noreply.github.com>
2023-05-29 16:32:28 +0000

    fe

---
+++ SortingAlgorithms/EfficientSortingAlgorithms/heapSort.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+/*
+
+Dado o array,
+
+int array[] = {2,0,9,8,1,5,4,3,15,7};
+
+Esse array pode ser reprensentada como uma árovere binária
+
+
+ex:                                            
+                                (2)
+                               /   \
+                              /     \
+                            (0)     (9)
+                           /  \      /  \
+                          /   (1)   (5)  (4)
+                        (8)   / 
+                        / \  (7)
+                      /    \
+                    (3)    (15)
+
+
+
+A regra é seguinte o nó pai, vai ter um filho esquerda com o index(2i+1) e pra direita com (2i+2)
+
+ex:
+
+o nó pai (2) tem o index(0) ele vai ter um filho pra direita seguindo regra(2i+2, 2(0)+2 = array[2], 9)
+
+o nó pai (2) tem o index(0) ele vai ter um filho pra esquerda seguindo regra(2i+1, 2(0)+1 = array[1], 0)
+
+e a mesma lógica vai ser aplicada para o resto dos nós 
+
+
+heap -> é uma árvore binária que obe
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
