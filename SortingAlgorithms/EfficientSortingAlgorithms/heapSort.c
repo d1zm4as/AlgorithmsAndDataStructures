5b42d42f7cd3a496b10d3a97c82dad52732c731c
d1zm4as <pedrofamilia364@gmail.com>
2023-06-04 13:00:36 -0300

    +heap

--- SortingAlgorithms/EfficientSortingAlgorithms/heapSort.c
+++ SortingAlgorithms/EfficientSortingAlgorithms/heapSort.c
@@ -34,16 +34,11 @@ o nó pai (2) tem o index(0) ele vai ter um filho pra esquerda seguindo regra(2i
 e a mesma lógica vai ser aplicada para o resto dos nós 
 
 
-heap -> é uma árvore binária que obe
+heap -> é uma árvore binária que obedece as seguintes condições ->
 
+1 - Todos os níveis com execessão do ultimo estão cheios
 
-
-
-
-
-
-
-
+2 - os valores dos nós pais não são menores que os valores do nós filhos
 
 
 
@@ -51,25 +46,75 @@ heap -> é uma árvore binária que obe
 */
 
 
+void swap(int* a, int* b)
+{
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 
+void heapify(int arr[], int N, int i)
+{
+    
+    int largest = i;
 
+    
+    int left = 2 * i + 1;
 
+    
+    int right = 2 * i + 2;
 
+    
+    if (left < N && arr[left] > arr[largest])
 
+        largest = left;
 
+    
+    if (right < N && arr[right] > arr[largest])
 
-int main(){
+        largest = right;
 
+    
+    if (largest != i) {
 
+        swap(&arr[i], &arr[largest]);
 
+        
+        heapify(arr, N, largest);
+    }
+}
 
+void heapSort(int arr[], int N)
+{
 
+    for (int i = N / 2 - 1; i >= 0; i--)
 
+        heapify(arr, N, i);
 
+    for (int i = N - 1; i >= 0; i--) {
 
+        swap(&arr[0], &arr[i]);
 
+        heapify(arr, i, 0);
+    }
+}
 
+void printArray(int arr[], int N)
+{
+    for (int i = 0; i < N; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
 
-    return 0;
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int N = sizeof(arr) / sizeof(arr[0]);
+
+    // Function call
+    heapSort(arr, N);
+    printf("Sorted array is\n");
+    printArray(arr, N);
 }
